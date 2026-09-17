ee7da2714d90bbbb5abbe3697595a23aa8c10aab
zue-pyae-paing <zuepyaepaing.dev@gmail.com>
2026-09-17 21:47:55 +0630

    fix: setup default admin creation

--- server/src/server.js
+++ server/src/server.js
@@ -3,21 +3,48 @@ require("dotenv").config();
 const http = require("http");
 const { Server } = require("socket.io");
 
+const bcrypt = require("bcryptjs");
+
 const app = require("./app");
 const connectDB = require("./config/db");
+const User = require("./models/user.model");
 const setupTokenSocket = require("./socket/token.socket");
 
 const PORT = process.env.PORT || 5000;
 
+const createDefaultAdmin = async () => {
+  const existingAdmin = await User.findOne({
+    username: "admin",
+  });
+
+  if (existingAdmin) {
+    console.log("Admin user already exists.");
+    return;
+  }
+
+  const password = await bcrypt.hash(
+    process.env.ADMIN_PASSWORD,
+    12
+  );
+
+  await User.create({
+    username: "admin",
+    password,
+    role: "admin",
+    isActive: true,
+  });
+
+  console.log("Default admin created successfully.");
+};
+
 const startServer = async () => {
   try {
-    // Connect MongoDB
     await connectDB();
 
-    // Create HTTP server
+    await createDefaultAdmin();
+
     const server = http.createServer(app);
 
-    // Setup Socket.IO
     const io = new Server(server, {
       cors: {
         origin: process.env.CLIENT_URL,
@@ -25,13 +52,10 @@ const startServer = async () => {
       },
     });
 
-    // Make Socket.IO available inside controllers
     app.set("io", io);
 
-    // Setup socket events
     setupTokenSocket(io);
 
-    // Start server
     server.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
       console.log("Socket.IO is ready");
@@ -39,7 +63,7 @@ const startServer = async () => {
   } catch (error) {
     console.error(
       "Server startup failed:",
-      error.message,
+      error.message
     );
 
     process.exit(1);
